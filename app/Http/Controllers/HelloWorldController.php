17a8ab6a8df895acd6b3c8c3f33206ed3274e232
valusasindhu <valusasindhu@gmail.com>
2024-11-27 16:36:00 +0530

    printing hellosindhu

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -10,25 +10,4 @@ class HelloWorldController extends Controller
     {
         return "Hello World";
     }
-
-    public function sum($num1,$num2)
-    {
-        return "Let's Calculate. Sum of " . $num1 . " and " . $num2 . " is " . ($num1 + $num2);
-
-    }
-    public function subtract($num1,$num2)
-    {
-        return "Let's Calculate. Subtraction of " . $num1 . " and " . $num2 . " is " . ($num1 - $num2);
-
-    }
-    public function multiply($num1,$num2)
-    {
-        return "Let's Calculate. Multiplication of " . $num1 . " and " . $num2 . " is " . ($num1 * $num2);
-
-    }
-    public function divide($num1,$num2)
-    {
-        return "Let's Calculate. Division of " . $num1 . " and " . $num2 . " is " . ($num1 / $num2);
-
-    }
-}
+};
