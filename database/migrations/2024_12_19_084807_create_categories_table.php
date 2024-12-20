98899518ffea217405f3cc1b5d2e3fae5de8c62c
valusasindhu <valusasindhu@gmail.com>
2024-12-20 16:39:27 +0530

    categories

--- database/migrations/2024_12_19_084807_create_categories_table.php
+++ database/migrations/2024_12_19_084807_create_categories_table.php
@@ -11,12 +11,10 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            Schema::create('categories', function (Blueprint $table) {
-                $table->id();
-                $table->string('title');
-                $table->string('slug')->unique();
-                $table->timestamps();
-            });
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->timestamps();
         });
     }
 
